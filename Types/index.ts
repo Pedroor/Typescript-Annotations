208b362720d29a805c96ff6c9ed2a30b118743d7
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-03-16 12:18:22 -0300

    Initial commit

---
+++ Types/index.ts
@@ -0,0 +1,6 @@
+const string: string = `texto ${1}`;
+const booleano: boolean = true;
+const numero: number = 10.5;
+const arr: string[] = ["text1", "text2"]; // ou
+const arr2: Array<string> = ["text1", "text2"];
+const tuple: [number, boolean] = [2, true];
